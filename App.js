17848ad434a507e832c61d67f2531b100cc5462c
Jason Brown <browniefed@gmail.com>
2018-01-31 19:39:10 -0800

    Add example code

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { PanResponder, StyleSheet, Text, View } from "react-native";
 
 export default class App extends Component {
-  
+  state = {
+    inactive: true,
+  };
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetPanResponderCapture: () => {
+        clearTimeout(this.timeout);
+
+        this.setState(state => {
+          if (state.inactive === false) return null;
+          return {
+            inactive: false,
+          };
+        });
+
+        this.timeout = setTimeout(() => {
+          this.setState({
+            inactive: true,
+          });
+        }, 3000);
+        return false;
+      },
+    });
+  }
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <View
+          style={[styles.activity, this.state.inactive ? styles.inactive : styles.active]}
+          {...this._panResponder.panHandlers}
+        >
+          <Text style={styles.text}>{this.state.inactive + ""}</Text>
+        </View>
       </View>
     );
   }
@@ -17,5 +50,21 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  activity: {
+    width: 200,
+    height: 80,
+    marginBottom: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  active: {
+    backgroundColor: "tomato",
+  },
+  inactive: {
+    backgroundColor: "black",
+  },
+  text: {
+    color: "#FFF",
   },
 });
