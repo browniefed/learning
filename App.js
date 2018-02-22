0969470d0a8bf74bde8bbb6d93f65342e0e2a8c0
Jason Brown <browniefed@gmail.com>
2018-02-21 21:36:58 -0800

    Fix start

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Animate } from "react-native";
+import { StyleSheet, Text, View, Animated } from "react-native";
 
 const BOX_DIM = 50;
 
