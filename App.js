ece5882e92ac5810b9e47d74ffeb7016d75cbe28
Jason Brown <browniefed@gmail.com>
2018-02-21 21:49:46 -0800

    Final code

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Animated } from "react-native";
+import { StyleSheet, Text, View, Animated, Dimensions } from "react-native";
 
 const BOX_DIM = 50;
+const { width, height } = Dimensions.get("window");
 
 export default class App extends Component {
+  
+  componentWillMount() {
+    this.animation = new Animated.ValueXY(0);
+  }
+  componentDidMount() {
+    Animated.parallel([
+      Animated.timing(this.animation.x, {
+        toValue: width * 2,
+        duration: 500
+      }),
+      Animated.timing(this.animation.y, {
+        toValue: height * 2,
+        duration: 500
+      })
+    ]).start()
+  }
+  
+  
   render() {
+
+    const widthRange = [0, width - BOX_DIM];
+    const heightRange = [0, height - BOX_DIM];
+
+    const xAnimation = this.animation.x.interpolate({
+      inputRange: widthRange,
+      outputRange: widthRange,
+      extrapolate: "clamp",
+    })
+
+    const yAnimation = this.animation.y.interpolate({
+      inputRange: heightRange,
+      outputRange: heightRange,
+      extrapolate: "clamp",
+    })
+
+    const moveStyle = {
+      transform: [
+        { translateX: xAnimation },
+        { translateY: yAnimation }
+      ]
+    }
     return (
       <View style={styles.container}>
-        <Animated.View style={styles.box} />
+        <Animated.View style={[styles.box, moveStyle]} />
       </View>
     );
   }
